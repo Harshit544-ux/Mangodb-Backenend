89405d04f2bbf74aeb3aa35cf25e4acce48f5343
Harshit <srivastava0798@gmail.com>
2025-06-15 14:36:34 +0530

    project setup

---
+++ app.js
@@ -0,0 +1,9 @@
+const express=require('express')
+const app=express();
+
+app.get('/',(req,res)=>{res.send('hello world')})
+
+
+const port=3000;
+app.listen(port,()=>console.log(`app is listening is at ${port}`))
+
